20e16ea4e4d12d552cef147946535f9cf51a1754
JonatasMRocha <jonatasrocha.pi@gmail.com>
2024-04-29 23:27:08 +0100

    booking page created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -28,7 +28,11 @@ const routes: Routes = [
   {
     path: 'reset-password',
     loadChildren: () => import('./pages/reset-password/reset-password.module').then( m => m.ResetPasswordPageModule)
+  },  {
+    path: 'booking',
+    loadChildren: () => import('./pages/booking/booking.module').then( m => m.BookingPageModule)
   },
+
 ];
 
 @NgModule({
